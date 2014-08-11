29b3f451d2ebddee901b1086170f8914717dde2f
josephhess <readysteadyco@gmail.com>
2014-08-11 16:59:35 -0700

    replace duplicate code with class dec's

--- bbq.h
+++ bbq.h
@@ -1,40 +1,56 @@
-#include "bbq.h"
-
-Auger::Auger (int p) { pin = p; }
-
-void Auger::turn(int rate = 0){
-    analogWrite(pin,rate);
-}
-    
-
-Fan::Fan (int p) { pin = p; }
-    
-void Fan::spin(int speed = 0){
-  analogWrite(pin,speed);
-} 
 
 
-Bbq::Bbq (int a){auger_pin = a;}
-Bbq::Bbq (int f){fan_pin = f;}
-Bbq::Bbq(){
-    Auger auger(auger_pin);
-    Fan   fan(fan_pin);  
-}
-
-    
-void Bbq::ignite(){
-    auger.turn(100);
-    fan.spin(100); 
-}
-
-
-void Bbq::cook(){
-    // feed pellets at rate specified by  
-    // spin fan at rate specified by
-    // keep reading temp
-    // keep reading temp2
-    // turn on ignition if needed
-}
-    
-void Bbq::keep_warm();
+class Auger{
+    int pin;
+    public:
 };
+
+class Fan{
+    int pin;
+    public:
+
+};
+class Igniter{
+    public:
+    void on();
+    void off();
+};
+
+class Bbq{
+    int a,f;
+    public:
+
+    void ignite();
+    void cook():
+    void keep_warm();
+};
+
+
+class Recipe{
+    // in manual mode, create recipe with 24 hours worth of time, otherwise use recipe indicated
+    // [[time,temp],[time,temp]]
+    
+};
+class Timer{
+    // initialize amount of time
+    // run amount of time(based on input of recipe)
+    public:
+    void start_cycle();
+    
+};
+
+
+
+// class Errors_if_i_can_read_from_device{
+//     void low_pellet(){}
+//     void no_ignition(){}
+//     void element_error(){}
+//     void auger_error(){}
+//     void fan_error(){}
+// };
+// start up heater, check for ingnition, if ignition start cooking , if not, restart ingnition sequence or some rescue sequence or alert of failure
+// heat sequence: activate fan and auger until temp block chosen has been reached. maintian temp by fan and auger cycle
+// check if reignition is needed, initiate if needed
+// run for time allotted or internal temp or related to recipe
+// watch for runaway temp
+// watch for internal temp
